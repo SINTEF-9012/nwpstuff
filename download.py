c764cc7786546a13e24c7b0d7776d75ebd2710e5
Volker Hoffmann <volker.hoffmann@sintef.no>
2022-04-26 13:53:23 +0200

    feat: download script

---
+++ download.py
@@ -0,0 +1,20 @@
+"""
+Download a given date for NWP product.
+
+Careful, lots of stuff is hardcoded in nwpstuff.download_nwp()!
+"""
+
+import nwpstuff
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--date', default='2019-01-01', 
+                    help='Date for NWP Data')
+parser.add_argument('-b', '--basedir', default='/tmp/vesselai',
+                    help='Base Directory for Files')
+parser.add_argument('--force', action='store_true', default=False)
+args = parser.parse_args()
+
+print("[*] Downloading NWP Product.")
+_ = nwpstuff.download_nwp(args.date, args.basedir, args.force)
